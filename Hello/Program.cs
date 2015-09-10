2f8b4be1cb7ec050de715e00b42d6e1e79ecea2d
svalw <Sonny.Vaupel@lampenwelt.de>
2015-09-10 14:05:01 +0200

    initial Test commit

---
+++ Hello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Console.ReadKey();
+        }
+
+        static void DoNothing() { }
+
+    }
+}
